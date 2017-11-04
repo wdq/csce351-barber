71efc03fe20578a7c1a652f9d1eb7cdb01d44e6d
William Quade <liam@quade.co>
2017-11-04 15:17:16 -0500

    Completed the first problem

---
+++ quade_hw3/prob1/README.txt
@@ -0,0 +1,47 @@
+- Team members: William Quade (I did this assignment alone)
+
+
+
+
+- To compile and execute my code type this into this directory:
+
+gcc -pthread sleepingStylistSem.c -o out && ./out
+
+
+
+
+- To test my code:
+
+Look at the output on the screen after the program has executed, here is an example line:
+
+B: waiting room customers count: 5, shopping customers count: 21, completed haircuts count: 6
+
+At the start there is a letter (B or C) to signal where the message is from.
+Messages starting with B come from the stylist/barber thread, messages starting with C come from a customer thread.
+
+Then there are three things logged. 
+
+1. Waiting room customer count: is the number of customers waiting in chairs. Ranges from 0-5. 
+2. Shopping customer count: is the number of customers who are waiting for a chair (shopping). 
+3. Completed haircut count: is the number of customers that have had their hair cut.
+
+Reading the output shows that there is a stream of customers who get into the chairs first.
+Once the chairs are full you will see the number of shopping customers increase to a maximum point, and then begin to decrease. 
+Throughout the whole program the completed haircut count should increase. 
+
+At the end of execution the last line should show 0 customers in the waiting room, 0 customers shopping, and 40 completed haircuts. 
+
+
+
+- Time spent on this code:
+
+I spent somewhere around 2 or 3 hours for this part of the assignment.
+
+
+
+- Level of challenge:
+
+3
+
+It took me a little while to find examples on how to use the sem_t and pthread stuff, and where I should print the debug output.
+But once I got that figured out it wasn't too difficult. The pseudo-code had all of the structure I needed.
--- quade_hw3/prob1/sleepingStylistSem.c
+++ quade_hw3/prob1/sleepingStylistSem.c
@@ -5,75 +5,106 @@
 
 #define CHAIRS 5
 #define DELAY 100000
-#define	CUSTOMER_COUNT	5
+#define	CUSTOMER_COUNT	40
 
+// Semaphores to keep track of some dangerous variables.
 sem_t mutex;
 sem_t stylistSem;
 sem_t customersSem;
 
-//semaphore mutex = 1, stylist = 0 customers = 0;
+// Some variables to keep track of how many people are where, used for debugging.
 int waiting = 0;
+int shopping = 0;
+int cut = 0;
 
+// Code the barber thread runs.
 void stylist() {
-	printf("I'm a stylist and I don't do anything yet\n");
-	/*int j;
+	int j;
 	while(1) {
-		down(&customers);
-		down(&mutex);
-		waiting = waiting - 1;
-		up($stylist);
-		up(&mutex);
 
+		// Print out debugging info.
+		printf("B: waiting room customers count: %i, shopping customers count: %i, completed haircuts count: %i\n", waiting, shopping, cut);		
+
+		// If everybody has had their hair cut, then leave the barber shop.
+		if(cut == CUSTOMER_COUNT) {
+			printf("All done\n");
+			break;
+		}		
+
+		// Take in a customer, and cut their hair.
+		sem_wait(&customersSem);
+		sem_wait(&mutex);
+		waiting = waiting - 1;
+		sem_post(&stylistSem);
+		sem_post(&mutex);
+
+		cut = cut + 1;
 		for(j = 0; j < DELAY; j++) {
-			// cut hair
+			// Cut hair, takes some time to do.
 		}
-	}*/
+	}
 }
 
+// Code the customer threads run.
 void customer() {
-	printf("I'm a customer and I don't do anything yet\n");
-	/*int j;
+	int hasShopped = 0;
+	int j;
 	while(1) {
-		down(&mutex);
+
+		// If there is a chair available, then take the chair.
+		sem_wait(&mutex);
 		if(waiting < CHAIRS) {
 			waiting = waiting + 1;
-			up(&customers);
-			up(&mutex);
-			down(&stylist);
+			// Print out debugging info.
+			printf("C: waiting room customers count: %i, shopping customers count: %i, completed haircuts count: %i\n", waiting, shopping, cut);
+
+			// If you've been shopping and enter the chair, then there is one less shopper.
+			if(hasShopped == 1) {
+				hasShopped = 0;
+				shopping = shopping - 1;
+			}
+			sem_post(&customersSem);
+			sem_post(&mutex);
+			sem_wait(&stylistSem);
 			break;
 		} else {
-			up(&mutex);
-
+			// Otherwise go shopping, and keep track of yourself if you are shopping for debugging.
+			if(hasShopped == 0) {
+				hasShopped = 1;
+				shopping = shopping + 1;
+			}
+			sem_post(&mutex);
 			for(j = 0; j < DELAY; j++) {
-				// Go shopping
+				// Go shopping, takes some time to do.
 			}
 		}
-	}*/
+	}
 }
 
 int main(void) {
-	// Create 40 customer threads and 1 stylist thread
-	// Don't forget to join threads
+
+	// Initialize the semaphores
+	sem_init(&mutex, 0, 1);
+	sem_init(&stylistSem, 0, 0);
+	sem_init(&customersSem, 0, 0);
+
+	// Create thread variables
 	pthread_t stylistThread;
 	pthread_t customerThread[CUSTOMER_COUNT];
 
 	// Create threads
 	pthread_create(&stylistThread, NULL, (void *)stylist, NULL);
-
 	for(int i = 0; i < CUSTOMER_COUNT; i++) {
 		pthread_create(&customerThread[i], NULL, (void *)customer, NULL);
 	}
 
-
 	// Join threads
-	pthread_join(stylistThread, NULL);
-
 	for(int i = 0; i < CUSTOMER_COUNT; i++ ) {
 		pthread_join(customerThread[i], NULL);
-	}
+	}	
+	pthread_join(stylistThread, NULL);
 
+	// Close when done with threads.
 	return 0;
-
-
 }
 
