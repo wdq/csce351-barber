b7b8e82fbc29c63381916a5f7e45b6f096310d25
William Quade <liam@quade.co>
2017-11-04 20:44:29 -0500

    Pseudocode for problem 2

--- quade_hw3/prob2/sleepingStylistMon.c
+++ quade_hw3/prob2/sleepingStylistMon.c
@@ -1,3 +1,39 @@
+// ==== monitor ==== //
+
+#define CHAIRS 5
+cond stylistAvailable, customerAvailable;
+int customer = 0;
+int stylist = 0;
+
+// add more variables as necessary (e.g. a semaphore for entry queue)
+
+void checkCustomer() {
+	stylist = stylist + 1;
+	signal(stylistAvailable); // stylist's ready to cut hair
+	if(customer == 0) { // do not use while here
+		wait(customerAvailable);
+	}
+	customer = customer - 1;
+}
+
+int checkStylist() {
+	// This function may have faults.
+	// If you think it does, you'll need to fix it.
+	int status = 0;
+	if(customer < CHAIRS) {
+		customer = customer + 1;
+		signal(customerAvailable);
+		if(stylist == 0) { // do not use while here
+			wait(stylistAvailable);
+		}
+		stylist = stylist - 1;
+		status = 1;
+	}
+	return status;
+}
+
+
+
 // ==== sleepingStylistMon ====//
 #define DELAY 100000 // adjust this value
 
@@ -17,7 +53,7 @@ void stylist() {
 	int j;
 	while(1) {
 		salonState();
-		mon.checkCustomer();
+		checkCustomer();
 		for(j = 0; j < DELAY; j++) {
 			// cut hair
 		}
@@ -29,7 +65,7 @@ void customer() {
 	int j;
 	while(1) {
 		salonState();
-		if(mon.checkStylist()) {
+		if(checkStylist()) {
 			break;
 		}
 		for(j = 0; j < DELAY; j++) {
@@ -38,4 +74,3 @@ void customer() {
 	}
 }
 
-// ==== monitor ==== //
